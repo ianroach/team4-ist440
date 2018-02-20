0d0acdf03ff345c58991b1f962d22495c56a20fc
Dzt5175 <35984507+Dzt5175@users.noreply.github.com>
2018-02-20 12:08:27 -0500

    Added person

--- Parking Mayhem/Assets/Scripts/PedestrainSpawner.cs
+++ Parking Mayhem/Assets/Scripts/PedestrainSpawner.cs
@@ -2,33 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarSpawner : MonoBehaviour {
+public class PedestrainSpawner : MonoBehaviour {
 
-	public float spawnDelay = 0.3f;
-
+	public float spawnDelay = 1.5f;
 	float nextTimeToSpawn = 0f;
 
-	public GameObject[] car;
+	public GameObject[]  pedestrian;
 
 	public Transform[] spawnPoints;
 
 	void Start () {
 		
 	}
-	
 
 	void Update () {
-		
 		if (nextTimeToSpawn <= Time.time) {
-			SpawnCar ();
+			SpawnPedestrian ();
 			nextTimeToSpawn = Time.time + spawnDelay;
 		}
 	}
-
-	void SpawnCar()
+	void SpawnPedestrian()
 	{
 		int randomIndex = Random.Range (0, spawnPoints.Length);
 		Transform spawnPoint = spawnPoints[randomIndex];
-		Instantiate (car[Random.Range(0, car.Length)], spawnPoint.position, spawnPoint.rotation);
+		Instantiate (pedestrian[Random.Range(0, pedestrian.Length)], spawnPoint.position, spawnPoint.rotation);
 	}
+
 }
