040746c6c4eab997fc3904dccb27c2fb9be05779
Dzt5175 <35984507+Dzt5175@users.noreply.github.com>
2018-02-20 08:48:42 -0500

    Spawner added

---
+++ Parking Mayhem/Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour {
+
+	public float spawnDelay = 0.3f;
+
+	float nextTimeToSpawn = 0f;
+
+	public GameObject car;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		if (nextTimeToSpawn <= Time.time) {
+			SpawnCar ();
+			nextTimeToSpawn = Time.time + spawnDelay;
+		}
+	}
+
+	void SpawnCar()
+	{
+		Instantiate (car);
+	}
+}
