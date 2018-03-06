190493322e1adebf3955f7217a9982baa1311870
Reynaldo Rodriguez <ReynaldoRodriguez26@gmail.com>
2018-03-06 00:17:07 -0500

    Midnight Update 3/6/18
    Added Menu U.I. and Functionality + Transition from one scene to next.

---
+++ Parking Mayhem/Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour {
+
+    public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false; /* Checks Application is running to false */
+        Application.Quit(); /* Quits Application*/
+    }
+}
---
+++ Parking Mayhem/Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class SelectOnInput : MonoBehaviour {
+
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw ("Vertical") !=0  && buttonSelected == false) /* Reads inputs from both keyboard or gamepad */
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true; /*Sets inputs entered to true in the boolean */
+        }
+	}
+
+    private void OnDisable()
+    {
+        buttonSelected = false; /* Any button selected will disable any other inputs*/
+    }
+}
