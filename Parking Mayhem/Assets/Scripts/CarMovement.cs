9f79f4e053cce7f0847211a1ad5a30326d395cfc
Danny <dthlang235@gmail.com>
2018-03-07 16:36:31 -0500

    Score Added

--- Parking Mayhem/Assets/Scripts/CarMovement.cs
+++ Parking Mayhem/Assets/Scripts/CarMovement.cs
@@ -2,81 +2,111 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
-
-public class CarMovement : MonoBehaviour {
-
-	public float power = 3;
-	public float maxspeed = 5;
-	public float turnpower = 2;
-	public float friction = 3;
-	public Vector2 curspeed ;
-     Rigidbody2D rigidbody2D;
-
-	// Use this for initialization
-	void Start () {
-		rigidbody2D = GetComponent<Rigidbody2D>();
-	}
+using UnityEngine.UI;
 
 
-	void FixedUpdate()
-	{
-		curspeed = new Vector2(rigidbody2D.velocity.x,    rigidbody2D.velocity.y);
+public class CarMovement : MonoBehaviour
+{
 
-		if (curspeed.magnitude > maxspeed)
-		{
-			curspeed = curspeed.normalized;
-			curspeed *= maxspeed;
-		}
+    public float power = 3;
+    public float maxspeed = 5;
+    public float turnpower = 2;
+    public float friction = 3;
+    public Vector2 curspeed;
+    public Text ScoreText;
+    public Vector3 StartPosition;
+    public Quaternion RotatePosition;
 
-		if (Input.GetKey(KeyCode.UpArrow))
-		{
-			rigidbody2D.AddForce(transform.up * power);
-			rigidbody2D.drag = friction;
-		}
-		if (Input.GetKey(KeyCode.DownArrow))
-		{
-			rigidbody2D.AddForce(-(transform.up) * (power/2));
-			rigidbody2D.drag = friction;
-		}
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.Rotate(Vector3.forward * turnpower);
-		}
-		if (Input.GetKey(KeyCode.RightArrow))
-		{
-			transform.Rotate(Vector3.forward * -turnpower);
-		}
+    new Rigidbody2D rigidbody2D;
+    private int count;
 
-		noGas();
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        StartPosition = transform.position;
+        RotatePosition = transform.rotation;
+        count = 0;
+        SetScoreText();
 
-	}
+    }
 
-	void noGas()
-	{
-		bool gas;
-		if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
-		{
-			gas = true;
-		}
-		else
-		{
-			gas = false;
-		}
 
-		if (!gas)
-		{
-			rigidbody2D.drag = friction * 2;
-		}
-	}
+    void FixedUpdate()
+    {
+        curspeed = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y);
+
+        if (curspeed.magnitude > maxspeed)
+        {
+            curspeed = curspeed.normalized;
+            curspeed *= maxspeed;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody2D.AddForce(transform.up * power);
+            rigidbody2D.drag = friction;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody2D.AddForce(-(transform.up) * (power / 2));
+            rigidbody2D.drag = friction;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * turnpower);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward * -turnpower);
+        }
+
+        noGas();
+
+    }
+
+    void noGas()
+    {
+        bool gas;
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            gas = true;
+        }
+        else
+        {
+            gas = false;
+        }
+
+        if (!gas)
+        {
+            rigidbody2D.drag = friction * 2;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Car")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Reset();
         }
         else if (collision.tag == "Gsol")
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+        else if (collision.gameObject.tag == "PickUp")
+        {
+            Destroy(collision.gameObject);
+            count += 50;
+            SetScoreText();
+        }
+    }
+    void SetScoreText()
+       
+    {
+        ScoreText.text = "Score: " + count.ToString();
+    }
+    private void Reset()
+    {
+        transform.position = StartPosition;
+        transform.rotation = RotatePosition;
     }
 }
