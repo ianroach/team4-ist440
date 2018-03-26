a5998392673f2255411cd8a8220e2ec5f40bcfe8
Dzt5175 <35984507+Dzt5175@users.noreply.github.com>
2018-03-26 14:00:42 -0400

    evening update

--- Parking Mayhem/Assets/Scripts/AlienMovement.cs
+++ Parking Mayhem/Assets/Scripts/AlienMovement.cs
@@ -8,6 +8,12 @@ public class AlienMovement : MonoBehaviour {
 	public GameObject target;
 	public float moveSpeed;
 	public float rotationSpeed;
+
+	public Transform SwitchCheck;
+	public float SwithRadius;
+	public LayerMask WhatIsSwitch;
+	private bool hittingSwitch;
+	public bool stopMove;
 	// Use this for initialization
 	void Start () {
 
@@ -15,12 +21,19 @@ public class AlienMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
-		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
-		Vector3 vectorToTarget = target.transform.position - transform.position;
-		float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
-		Quaternion qt = Quaternion.AngleAxis(angle, Vector3.forward);
-		transform.rotation = Quaternion.RotateTowards(transform.rotation, qt, Time.deltaTime * rotationSpeed);
+
+		hittingSwitch = Physics2D.OverlapCircle (SwitchCheck.position, SwithRadius, WhatIsSwitch);
+		if (hittingSwitch)
+			stopMove = !stopMove;
+		if (stopMove) {
+			transform.position = new Vector3 (0f, 0f, 0f);
+		} else {
+			transform.position = Vector3.MoveTowards (transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+			Vector3 vectorToTarget = target.transform.position - transform.position;
+			float angle = Mathf.Atan2 (vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+			Quaternion qt = Quaternion.AngleAxis (angle, Vector3.forward);
+			transform.rotation = Quaternion.RotateTowards (transform.rotation, qt, Time.deltaTime * rotationSpeed);
+		}
 	}
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
