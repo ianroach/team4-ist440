0bf848e9e3a66fdf487c8c68ac58bc2900825bf8
Dzt5175 <35984507+Dzt5175@users.noreply.github.com>
2018-04-05 16:30:17 -0400

    update

--- Parking Mayhem/Assets/Scripts/Goal.cs
+++ Parking Mayhem/Assets/Scripts/Goal.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Goal: MonoBehaviour {
 
     // Use this for initialization
     private void OnTriggerEnter2D(Collider2D collision)
