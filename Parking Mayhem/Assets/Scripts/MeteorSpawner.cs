f2195dac4945b2813b8b32c0ca0c641b86b9cc86
Reynaldo Rodriguez <ReynaldoRodriguez26@gmail.com>
2018-04-08 21:38:36 -0400

    Night Update 4/8/18
    Added spawner and meteor object.

---
+++ Parking Mayhem/Assets/Scripts/MeteorSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawner : MonoBehaviour {
+
+    public float spawnDelay = 0.3f;
+
+    float nextTimeToSpawn = 0f;
+
+    public GameObject[] Meteor;
+
+    public Transform[] spawnPoints;
+
+    void Start()
+    {
+
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+
+        if (nextTimeToSpawn <= Time.time)
+        {
+            SpawnMeteor();
+            nextTimeToSpawn = Time.time + spawnDelay;
+        }
+    }
+
+    void SpawnMeteor()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+        Transform spawnPoint = spawnPoints[randomIndex];
+        Instantiate(Meteor[Random.Range(0, Meteor.Length)], spawnPoint.position, spawnPoint.rotation);
+    }
+}
+
