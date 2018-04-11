1440d2cfd8439cc70bd31948696d9fe9d1d2af18
Danny <dthlang235@gmail.com>
2018-04-10 22:31:53 -0400

    Kill me please

---
+++ Parking Mayhem/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+
+
+    private ScoreManager score;
+
+    public Text ScoreUIText;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        score = GameObject.FindGameObjectWithTag("ScoreManager").GetComponent<ScoreManager>();
+        ScoreUIText.text = "" + score.count.ToString();
+    }
+
+}
