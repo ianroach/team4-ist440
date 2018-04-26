f9bfbb22d9b7c9077afc6b09a63386b39312344f
Reynaldo Rodriguez <ReynaldoRodriguez26@gmail.com>
2018-04-25 23:42:09 -0400

    V3.3.2 Update
    Added ability to change audio sounds in main menu.

---
+++ Parking Mayhem/Assets/Scripts/AudioVolume.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVolume : MonoBehaviour {
+
+
+    float value = 100f;
+    AudioSource source;
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void SetVolume(float value)
+    {
+        source.volume = value;
+    }
+}
