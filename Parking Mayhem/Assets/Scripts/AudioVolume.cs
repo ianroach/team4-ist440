aef945a728619f8e2a7fccfd304065fec82c1ece
Reynaldo Rodriguez <ReynaldoRodriguez26@gmail.com>
2018-04-26 00:03:26 -0400

    V3.4.0 Update
    Fix for break build.

--- Parking Mayhem/Assets/Scripts/AudioVolume.cs
+++ Parking Mayhem/Assets/Scripts/AudioVolume.cs
@@ -6,7 +6,7 @@ public class AudioVolume : MonoBehaviour {
 
 
     float value = 100f;
-    AudioSource source;
+   public static AudioSource source;
     // Use this for initialization
     void Start()
     {
